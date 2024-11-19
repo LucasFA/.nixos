4098438aaa27b9534a8c8e77f48d148a3c923897
LucasFA <23667494+LucasFA@users.noreply.github.com>
2024-11-19 16:09:33 +0000

    Use my gh:nixos-hardware config

--- hosts/slimbook-hero/default.nix
+++ hosts/slimbook-hero/default.nix
@@ -1,6 +1,14 @@
 { config, lib, pkgs, ... }:
 
 {
+  imports = [
+    "${builtins.fetchGit { 
+	url = "https://github.com/LucasFA/nixos-hardware.git"; 
+	ref = "master";
+	rev = "1d007a7c3992a260d4379e8e368a3f9305bcf4cd";
+	}}/slimbook/hero_rpl_rtx"
+    ./nvidia.nix
+  ];
   fileSystems."/mnt/data" = 
     { device = "/dev/disk/by-uuid/0cf1fffe-2c1f-4c9a-b29b-5d63d22dd602";
       fsType = "ext4";
