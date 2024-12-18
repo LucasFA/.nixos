de2b0513affd52de9dc9e2be69a8fd9e43e6c081
LucasFA <23667494+LucasFA@users.noreply.github.com>
2024-12-18 23:03:25 +0000

    Make fish default interactive shell

--- home/default.nix
+++ home/default.nix
@@ -26,6 +26,16 @@
 	chezmoi
   ];
   programs = {
+    bash = {
+	enable = true;
+	 interactiveShellInit = ''
+    if [[ $(${pkgs.procps}/bin/ps --no-header --pid=$PPID --format=comm) != "fish" && -z ''${BASH_EXECUTION_STRING} ]]
+    then
+      shopt -q login_shell && LOGIN_OPTION='--login' || LOGIN_OPTION=""
+      exec ${pkgs.fish}/bin/fish $LOGIN_OPTION
+    fi
+  '';
+  };
     fish.enable = true;
     zed-editor = {
       enable = true;
