b64fde5d31789040263500ec6931f7784d800541
LucasFA <23667494+LucasFA@users.noreply.github.com>
2024-12-18 20:45:52 +0000

    Bootloader config timeout =2, confLimit = 25

--- configuration.nix
+++ configuration.nix
@@ -17,9 +17,11 @@
   boot.loader.systemd-boot.enable = false;
   boot.loader.efi.canTouchEfiVariables = true;
   boot.loader.efi.efiSysMountPoint = "/boot/efi";
+  boot.loader.timeout = 2;
   boot.loader.grub = {
 	enable = true;
         copyKernels = false; # no need: uses them straight from /nix/store
+	configurationLimit = 25;
 	useOSProber = true;
 	efiSupport = true;
      	#efiInstallAsRemovable = true; # in case canTouchEfiVariables doesn't work for your system
