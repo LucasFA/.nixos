9c93a6cbc0f81da45befc1e6267ae69caf47bb96
LucasFA <23667494+LucasFA@users.noreply.github.com>
2024-12-18 00:48:54 +0000

    Btrfs dedup and scrub

--- hosts/slimbook-hero/default.nix
+++ hosts/slimbook-hero/default.nix
@@ -17,7 +17,15 @@
      fsType = "btrfs";
      neededForBoot = true;
      options = [ "noatime" ];
-};
+  };
+  services.beesd.filesystems.nix_store = {
+      spec = "LABEL=Nix";
+  };
+  services.btrfs.autoScrub = {
+    enable = true;
+    fileSystems = [ "/nix/" ];
+    interval = "1months";
+  };
   fileSystems."/mnt/data" = 
     { device = "/dev/disk/by-uuid/0cf1fffe-2c1f-4c9a-b29b-5d63d22dd602";
       fsType = "ext4";
