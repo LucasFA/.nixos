a1676f64f7b7a95a16e707b93c03f18050490ac3
LucasFA <23667494+LucasFA@users.noreply.github.com>
2024-12-19 00:20:02 +0000

    nixd, nixfmt

--- home/default.nix
+++ home/default.nix
@@ -4,6 +4,7 @@
 	./git
 	./zed
 	./shell
+	./devtools
   ];
   # Home Manager needs a bit of information about you and the
   # paths it should manage.
--- installed_programs.nix
+++ installed_programs.nix
@@ -55,5 +55,8 @@
   #  wget
     dmidecode
     lshw
+
+    nixfmt-rfc-style
+    nixd
   ];
 }
