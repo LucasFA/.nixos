8d7020566844c186e135854ac936e4665e6ed0fa
LucasFA <23667494+LucasFA@users.noreply.github.com>
2024-12-20 23:11:23 +0000

    Use nixos-hardware module instead of fetchGit (commented out)

--- hosts/slimbook-hero/default.nix
+++ hosts/slimbook-hero/default.nix
@@ -5,13 +5,7 @@
 
 {
   imports = [
-    "${
-      builtins.fetchGit {
-        url = "https://github.com/LucasFA/nixos-hardware.git";
-        ref = "slimbook_hero";
-        rev = "af392dd02020c0db28b23d8532f02f594708d202";
-      }
-    }/slimbook/hero/rpl_rtx"
+    # nixos-hardware.nixosModules.slimbook-hero-rpl-rtx
     ./nvidia.nix
     ./filesystems.nix
     ./zram.nix
