3c58c5c5a455a75d4a3b274e1be67509a59b3af5
LucasFA <23667494+LucasFA@users.noreply.github.com>
2024-12-20 23:10:12 +0000

    Move boot configuration into hosts/slimbook-hero

--- configuration.nix
+++ configuration.nix
@@ -20,33 +20,6 @@
     ./nix.nix
   ];
 
-  # Bootloader.
-  boot.loader.systemd-boot.enable = false;
-  boot.loader.efi.canTouchEfiVariables = true;
-  boot.loader.efi.efiSysMountPoint = "/boot/efi";
-  boot.loader.timeout = 2;
-  boot.loader.grub = {
-    enable = true;
-    copyKernels = false; # no need: uses them straight from /nix/store
-    configurationLimit = 25;
-    useOSProber = true;
-    efiSupport = true;
-    #efiInstallAsRemovable = true; # in case canTouchEfiVariables doesn't work for your system
-    device = "nodev";
-    font = "${pkgs.fira-code}/share/fonts/truetype/FiraCode-VF.ttf";
-    fontSize = 36;
-    extraEntries = ''
-                      menuentry "Reboot" {
-                          reboot
-                      }
-       		menuentry "Poweroff" {
-                          halt
-                      }
-      		'';
-  };
-
-  boot.kernelParams = [
-  ];
   networking.hostName = "nixos"; # Define your hostname.
   # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
 
