aa6295d620b8008ecadbd6bbbdd30cd7d1507222
LucasFA <23667494+LucasFA@users.noreply.github.com>
2024-12-22 18:46:17 +0000

    Clean up nixos-hardware configuration

--- hosts/slimbook-hero/nvidia.nix
+++ hosts/slimbook-hero/nvidia.nix
@@ -1,40 +1,18 @@
 {
   pkgs,
   config,
-  libs,
+  lib,
   ...
 }:
-
 {
-
   hardware.graphics = {
     enable = true;
     enable32Bit = true;
   };
 
-  # Load nvidia driver for Xorg and Wayland
   # services.xserver.videoDrivers = ["nvidia"]; #implied by nixos-hardware
 
   hardware.nvidia = {
-    # modesetting.enable = true; # set by nixos-hardware. Required.
-
-    # Nvidia power management. Experimental, and can cause sleep/suspend to fail.
-    powerManagement = {
-      enable = true;
-      # Fine-grained power management. Turns off GPU when not in use.
-      # Experimental and only works on modern Nvidia GPUs (Turing or newer).
-      finegrained = true;
-    };
-    dynamicBoost.enable = true;
-
-    # Use the NVidia open source kernel module (not to be confused with the
-    # independent third-party "nouveau" open source driver).
-    # Support is limited to the Turing and later architectures. Full list of
-    # supported GPUs is at:
-    # https://github.com/NVIDIA/open-gpu-kernel-modules#compatible-gpus
-
-    # open = true; # implied by nixos-hardware
-
     # Enable the Nvidia settings menu,
     # accessible via `nvidia-settings`.
     nvidiaSettings = true;
@@ -42,5 +20,4 @@
     # Optionally, you may need to select the appropriate driver version for your specific GPU.
     package = config.boot.kernelPackages.nvidiaPackages.stable;
   };
-
 }
