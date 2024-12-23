c79beb5b5f5de195cfab0a01b643550bf9b1938a
LucasFA <23667494+LucasFA@users.noreply.github.com>
2024-12-23 22:24:12 +0000

    Install vscode

---
+++ home/vscode/default.nix
@@ -0,0 +1,15 @@
+{
+  pkgs,
+  ...
+}:
+{
+programs.vscode = {
+  enable = true;
+  package = pkgs.vscodium;
+  extensions = with pkgs.vscode-extensions; [
+    jnoortheen.nix-ide
+    vscodevim.vim
+    yzhang.markdown-all-in-one
+  ];
+};
+}
