50fc1b4aececac480034de1d59f3ab6721988e7d
LucasFA <23667494+LucasFA@users.noreply.github.com>
2024-12-23 22:32:18 +0000

    format

--- home/vscode/default.nix
+++ home/vscode/default.nix
@@ -3,13 +3,13 @@
   ...
 }:
 {
-programs.vscode = {
-  enable = true;
-  package = pkgs.vscodium;
-  extensions = with pkgs.vscode-extensions; [
-    jnoortheen.nix-ide
-    vscodevim.vim
-    yzhang.markdown-all-in-one
-  ];
-};
+  programs.vscode = {
+    enable = true;
+    package = pkgs.vscodium;
+    extensions = with pkgs.vscode-extensions; [
+      jnoortheen.nix-ide
+      vscodevim.vim
+      yzhang.markdown-all-in-one
+    ];
+  };
 }
