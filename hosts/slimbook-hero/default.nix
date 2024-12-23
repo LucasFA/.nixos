ac27c2d5b9534c0ab2c89e3bd00d56fc8ebddfaf
LucasFA <23667494+LucasFA@users.noreply.github.com>
2024-12-23 00:22:03 +0000

    Factor out boot conf

--- hosts/slimbook-hero/default.nix
+++ hosts/slimbook-hero/default.nix
@@ -7,35 +7,10 @@
   imports = [
     # nixos-hardware.nixosModules.slimbook-hero-rpl-rtx
     ./gnome-fix.nix
+    ./boot.nix
     ./nvidia.nix
     ./filesystems.nix
     ./zram.nix
   ];
 
-  boot.loader.systemd-boot.enable = false;
-  boot.loader.efi.canTouchEfiVariables = true;
-  boot.loader.efi.efiSysMountPoint = "/boot/efi";
-  boot.loader.timeout = 2;
-  boot.loader.grub = {
-    enable = true;
-    copyKernels = false; # no need: uses them straight from /nix/store
-    configurationLimit = 25;
-    useOSProber = true;
-    efiSupport = true;
-    #efiInstallAsRemovable = true; # in case canTouchEfiVariables doesn't work for your system
-    device = "nodev";
-    font = "${pkgs.fira-code}/share/fonts/truetype/FiraCode-VF.ttf";
-    fontSize = 36;
-    extraEntries = ''
-                      menuentry "Reboot" {
-                          reboot
-                      }
-       		menuentry "Poweroff" {
-                          halt
-                      }
-      		'';
-  };
-
-  boot.kernelParams = [
-  ];
 }
