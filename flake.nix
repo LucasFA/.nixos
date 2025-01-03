d5cff50358029336d795873ad3986573ad80fc7a
LucasFA <23667494+LucasFA@users.noreply.github.com>
2025-01-03 19:46:14 +0000

    Use my own nixpkgs fork

--- flake.nix
+++ flake.nix
@@ -2,7 +2,7 @@
   description = "My first flake!";
 
   inputs = {
-    nixpkgs.url = "nixpkgs/nixos-24.11";
+    nixpkgs.url = "github:LucasFA/nixpkgs/testing";
     home-manager = {
       url = "github:nix-community/home-manager/release-24.11";
       inputs.nixpkgs.follows = "nixpkgs";
