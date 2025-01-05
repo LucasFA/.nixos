ca24785f00fcfbe050778ff853be6c3eb69e10b7
LucasFA <23667494+LucasFA@users.noreply.github.com>
2025-01-05 12:08:08 +0000

    Use home-manager 24.11

--- flake.nix
+++ flake.nix
@@ -4,7 +4,7 @@
   inputs = {
     nixpkgs.url = "nixpkgs/nixos-24.11";
     home-manager = {
-      url = "github:nix-community/home-manager";
+      url = "github:nix-community/home-manager/release-24.11";
       inputs.nixpkgs.follows = "nixpkgs";
     };
     nixos-hardware.url = "github:LucasFA/nixos-hardware/slimbook_hero";
