6bb9e35ae214bc2f6bdb8c452efcad0311837db8
LucasFA <23667494+LucasFA@users.noreply.github.com>
2025-01-06 20:18:08 +0000

    Use local nixpkgs, enable qc71

--- flake.nix
+++ flake.nix
@@ -2,7 +2,8 @@
   description = "My first flake!";
 
   inputs = {
-    nixpkgs.url = "github:LucasFA/nixpkgs/testing-nixos-24.11";
+    nixpkgs.url = "git+file:///home/lucasfa/dev/nixpkgs?ref=testing-nixos-24.11";
+    #nixpkgs.url = "github:LucasFA/nixpkgs/testing-nixos-24.11";
     home-manager = {
       url = "github:nix-community/home-manager/release-24.11";
       inputs.nixpkgs.follows = "nixpkgs";
