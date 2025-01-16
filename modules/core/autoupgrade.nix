cb553fd9d3b9af0826f83a57da82159c0240ea1c
LucasFA <23667494+LucasFA@users.noreply.github.com>
2025-01-16 14:49:14 +0000

    Fix autoupgrade: only upgrade to gh repo lockfile

--- modules/core/autoupgrade.nix
+++ modules/core/autoupgrade.nix
@@ -8,9 +8,6 @@
     enable = lib.mkDefault true;
     flake = "github:LucasFA/.nixos";
     flags = [
-      "--update-input"
-      "nixpkgs"
-      "--commit-lock-file"
       "--print-build-logs"
     ];
     randomizedDelaySec = "15min";
