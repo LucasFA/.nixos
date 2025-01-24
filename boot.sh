965dc4d5dc5d0c8e39d417df01685c1e2425877a
LucasFA <23667494+LucasFA@users.noreply.github.com>
2025-01-24 21:04:25 +0000

    Add shellcheck to nix fmt

--- boot.sh
+++ boot.sh
@@ -23,7 +23,7 @@ git diff -U0 '*.nix'
 echo "NixOS Rebuilding..."
 
 # Rebuild, output simplified errors, log trackebacks
-nixos-rebuild boot &>nixos-switch.log || (cat nixos-switch.log | grep --color error && exit 1)
+nixos-rebuild boot &>nixos-switch.log || (grep --color error nixos-switch.log && exit 1)
 
 # Get current generation metadata
 current=$(nixos-rebuild list-generations | grep current)
--- modules/treefmt/default.nix
+++ modules/treefmt/default.nix
@@ -3,6 +3,7 @@
   # Used to find the project root
   projectRootFile = "flake.nix";
   programs.nixfmt.enable = true;
+  programs.shellcheck.enable = true;
   # Enable the terraform formatter
   #programs.terraform.enable = true;
   # Override the default package
