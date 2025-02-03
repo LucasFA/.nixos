f141f136cdf157cd2fd975a4fe9ce9ec176520f7
LucasFA <23667494+LucasFA@users.noreply.github.com>
2025-02-03 21:19:57 +0000

    Use gnome automatic-timezone

--- hosts/form-factor/laptop/default.nix
+++ hosts/form-factor/laptop/default.nix
@@ -1,4 +1,5 @@
 { lib, ... }:
 {
-  services.automatic-timezoned.enable = lib.mkDefault true;
+  # services.automatic-timezoned.enable = lib.mkDefault true; # never got it to work
+  time.timeZone = lib.mkForce null; # set by user
 }
