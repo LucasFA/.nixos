39b451dcdc056af071a433ec8a5dcc6da4895ba6
LucasFA <23667494+LucasFA@users.noreply.github.com>
2025-02-04 17:23:29 +0000

    woops

--- modules/borgbackup/server.nix
+++ modules/borgbackup/server.nix
@@ -5,16 +5,18 @@
 }:
 let
   slimbook_borg_ssh_key = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIM5kC93c4adHZUbRiSiGNAhb48UEQF73UoLsBg1SWT5j root@slimbook Borg backup of /home/lucasfa";
+  mkRepo = path: {
+    authorizedKeys = [
+      slimbook_borg_ssh_key
+    ];
+    path = path;
+  };
 in
 {
   programs.ssh.knownHosts."slimbook" = {
     hostNames = [ "slimbook" ];
     publicKey = slimbook_borg_ssh_key;
   };
-  services.borgbackup.repos.slimbook = {
-    authorizedKeys = [
-      slimbook_borg_ssh_key
-    ];
-    path = "/mnt/WD_8tb/server/data/borg/slimbook";
-  };
+  services.borgbackup.repos.slimbook = mkRepo "/mnt/WD_8tb/server/data/borg/slimbook";
+  services.borgbackup.repos.home-lucasfa = mkRepo "/mnt/WD_8tb/server/data/borg/home-lucasfa-slimbook";
 }
