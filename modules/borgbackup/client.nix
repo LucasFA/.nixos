1d5e7481ad720a88a3c1a15a45ddb38c4f646037
LucasFA <23667494+LucasFA@users.noreply.github.com>
2025-02-04 00:22:26 +0000

    borg client conf

--- modules/borgbackup/client.nix
+++ modules/borgbackup/client.nix
@@ -5,6 +5,12 @@
 }:
 let
   excludeList = [
+    ".cache"
+    "*/cache2" # firefox
+    "*/Cache"
+    ".config/Code/CachedData"
+    ".npm/_cacache"
+    "*/node_modules"
     "*.pyc"
     "/home/lucasfa/dev/***/[.]git"
     "/home/lucasfa/games"
@@ -25,14 +31,21 @@ in
     paths = [ "/home/lucasfa" ];
     exclude = excludeList;
     doInit = true;
-    repo = "borg@hp-server-omen:/mnt/WD_8tb/server/data/borg/home-lucasfa-slimbook";
+    repo = "ssh://borg@server-hp-omen:22/mnt/WD_8tb/server/data/borg/home-lucasfa-slimbook";
     encryption = {
       mode = "repokey-blake2";
       passphrase = "prolonged ranting unhinge surviving herself energetic grievous reimburse trophy undermost enrage outmost";
     };
     environment.BORG_RSH = "ssh -i /home/lucasfa/.ssh/id_ed25519_borg_home_lucasfa";
+    environment.BORG_EXIT_CODES = "modern";
     compression = "auto,zstd";
 
-    startAt = "daily";
+    startAt = "hourly";
+    prune.keep = {
+      within = "3d";
+      daily = 7;
+      weekly = 4;
+      monthly = 2;
+    };
   };
 }
