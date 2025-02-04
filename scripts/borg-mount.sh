3280928cceaee42fe2a4ed53953242c663e6a68d
LucasFA <23667494+LucasFA@users.noreply.github.com>
2025-02-04 13:16:09 +0000

    Add borg scripts, create ./scripts folder

---
+++ scripts/borg-mount.sh
@@ -0,0 +1,1 @@
+mkdir borgtest && borg-job-home-lucafa mount borg@server-hp-omen:/mnt/WD_8tb/server/data/borg/home-lucasfa-slimbook ./borgtest/
---
+++ scripts/borg-umount.sh
@@ -0,0 +1,1 @@
+borg-job-home-lucafa umount borgtest && rmdir borgtest
