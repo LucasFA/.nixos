f5cdb9ca327f88037b8d4c020b32d655688d3689
LucasFA <23667494+LucasFA@users.noreply.github.com>
2025-02-04 16:56:18 +0000

    borg refactor

--- modules/borgbackup/client.nix
+++ modules/borgbackup/client.nix
@@ -4,51 +4,65 @@
   ...
 }:
 let
-  excludeList = [
-    ".cache"
-    "*/cache2" # firefox
-    "*/Cache"
-    ".config/Code/CachedData"
-    ".npm/_cacache"
-    "*/node_modules"
-    "*.pyc"
-    "/home/lucasfa/dev/***/[.]git"
-    "/home/lucasfa/games"
-    "/home/*/.direnv"
-    "/home/*/.cache"
-    "/home/*/.cargo"
-    "/home/*/.npm"
-    "/home/*/.mozilla/firefox/*/storage"
-    "/var/lib/docker/"
-    "/var/log/journal"
-    "/var/cache"
-    "/var/tmp"
-    "/var/log"
-  ];
+  excludeList =
+    [
+      "*/cache2" # firefox
+      "*/Cache"
+      ".config/Code/CachedData"
+      ".npm/_cacache"
+      "*/node_modules"
+      "*.pyc"
+      "/home/lucasfa/dev/***/[.]git"
+      "/home/lucasfa/games"
+      "/home/*/.direnv"
+      "/home/*/.cache"
+      "/home/*/.cargo"
+      "/home/*/.npm"
+      "/home/*/.mozilla/firefox/*/storage"
+    ]
+    ++ [
+      "/root/.cache"
+      "/var/lib/docker"
+      "/var/log/journal"
+      "/var/cache"
+      "/var/tmp"
+      "/var/log"
+    ];
+  mkDailyBorgJob = paths: mkBorgJob { paths = paths; };
+  mkBorgJob =
+    {
+      paths,
+      startAt ? "daily",
+    }:
+    {
+      paths = paths;
+      exclude = excludeList;
+      doInit = true;
+      repo = "ssh://borg@server-hp-omen:22/mnt/WD_8tb/server/data/borg/home-lucasfa-slimbook";
+      encryption = {
+        mode = "repokey-blake2";
+        passphrase = "prolonged ranting unhinge surviving herself energetic grievous reimburse trophy undermost enrage outmost";
+      };
+      environment.BORG_RSH = "ssh -i /home/lucasfa/.ssh/id_ed25519_borg_home_lucasfa";
+      environment.BORG_EXIT_CODES = "modern";
+      compression = "auto,zstd";
+
+      startAt = startAt;
+      prune.keep = {
+        within = "3d";
+        daily = 7;
+        weekly = 4;
+        monthly = 2;
+      };
+    };
 in
 {
   imports = [
     ./persistent.nix
   ];
-  services.borgbackup.jobs.home-lucafa = {
-    paths = [ "/home/lucasfa" ];
-    exclude = excludeList;
-    doInit = true;
-    repo = "ssh://borg@server-hp-omen:22/mnt/WD_8tb/server/data/borg/home-lucasfa-slimbook";
-    encryption = {
-      mode = "repokey-blake2";
-      passphrase = "prolonged ranting unhinge surviving herself energetic grievous reimburse trophy undermost enrage outmost";
-    };
-    environment.BORG_RSH = "ssh -i /home/lucasfa/.ssh/id_ed25519_borg_home_lucasfa";
-    environment.BORG_EXIT_CODES = "modern";
-    compression = "auto,zstd";
-
-    startAt = "daily";
-    prune.keep = {
-      within = "3d";
-      daily = 7;
-      weekly = 4;
-      monthly = 2;
-    };
-  };
+  services.borgbackup.jobs.home-lucafa = mkDailyBorgJob [ "/home/lucasfa" ];
+  services.borgbackup.jobs.system = mkDailyBorgJob [
+    "/var"
+    "/root"
+  ];
 }
