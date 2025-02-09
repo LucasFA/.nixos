9deda2d8f92df009b0778ce4cbfbd052a965bba3
LucasFA <23667494+LucasFA@users.noreply.github.com>
2025-02-09 19:56:06 +0000

    forgejo: declarative config instead

--- modules/forgejo/default.nix
+++ modules/forgejo/default.nix
@@ -5,6 +5,10 @@
   services.forgejo = {
     enable = true;
     stateDir = "/mnt/WD_8tb/server/data/forgejo";
-    useWizard = true;
+    useWizard = false;
+    settings = {
+      server.DOMAIN = "git.lucasfa.com";
+      session.COOKIE_SECURE = true;
+    };
   };
 }
