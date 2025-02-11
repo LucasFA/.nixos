8a614a62ef9f1a0b3e2b9a938433ebd4b2ef548c
LucasFA <23667494+LucasFA@users.noreply.github.com>
2025-02-11 16:21:58 +0000

    no-op: rename setting, from warning

--- hosts/slimbook-hero/software/default.nix
+++ hosts/slimbook-hero/software/default.nix
@@ -27,8 +27,8 @@
   system.stateVersion = "24.05"; # Did you read the comment?
 
   # Enable automatic login for the user.
-  services.xserver.displayManager.autoLogin.enable = true;
-  services.xserver.displayManager.autoLogin.user = "lucasfa";
+  services.displayManager.autoLogin.enable = true;
+  services.displayManager.autoLogin.user = "lucasfa";
 
   # Workaround for GNOME autologin: https://github.com/NixOS/nixpkgs/issues/103746#issuecomment-945091229
   systemd.services."getty@tty1".enable = false;
