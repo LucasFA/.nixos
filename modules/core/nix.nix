8f7ecb309126eee2e9072e138e86ce56413f9091
LucasFA <23667494+LucasFA@users.noreply.github.com>
2025-02-12 20:51:02 +0000

    no-op: refactor nix config

--- modules/core/nix.nix
+++ modules/core/nix.nix
@@ -4,30 +4,35 @@
   ...
 }:
 {
-  nix.settings = {
-    keep-outputs = true;
-    keep-derivations = true;
-    experimental-features = [
-      "nix-command"
-      "flakes"
-    ];
-    # remove channels, use flakes
-    # registry.nixpkgs.flake = "nixpkgs";
-    #channel.enable = false;
-    nix-path = lib.mkForce "nixpkgs=/etc/nix/inputs/nixpkgs";
-  };
-  # https://github.com/NixOS/nix/issues/9574 # part of removing channels
-  #environment.etc."nix/inputs/nixpkgs".source = "${nixpkgs}";
 
-  nix.gc = {
-    automatic = true;
-    dates = "weekly";
-    persistent = true;
-    randomizedDelaySec = "30min";
-    options = "--delete-older-than 30d --max-jobs 2";
-  };
-  nix.optimise = {
-    automatic = true;
-    dates = [ "monthly" ];
+  nix = {
+    settings = {
+      keep-outputs = true;
+      keep-derivations = true;
+      experimental-features = [
+        "nix-command"
+        "flakes"
+      ];
+      nix-path = lib.mkForce "nixpkgs=/etc/nix/inputs/nixpkgs";
+    };
+    # registry.nixpkgs.flake = "nixpkgs";
+    # srvos config disables channels. Reenable to fix DBI sqlite error on command-not-found
+    # and then, as root, add the nixos-unstable channel (nix-channel --add https://...)
+    # and then update it (nix-channel --update)
+    # channel.enable = true;
+    # https://github.com/NixOS/nix/issues/9574 # part of removing channels
+    #environment.etc."nix/inputs/nixpkgs".source = "${nixpkgs}";
+
+    gc = {
+      automatic = true;
+      dates = "weekly";
+      persistent = true;
+      randomizedDelaySec = "30min";
+      options = "--delete-older-than 14d --max-jobs 2";
+    };
+    optimise = {
+      automatic = true;
+      dates = [ "monthly" ];
+    };
   };
 }
