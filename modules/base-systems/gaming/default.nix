25b07d250215adaf38bf64fbb641b5a8facd9f67
LucasFA <23667494+LucasFA@users.noreply.github.com>
2025-04-16 10:54:56 +0100

    gamescope ...(?)

--- modules/base-systems/gaming/default.nix
+++ modules/base-systems/gaming/default.nix
@@ -1,8 +1,31 @@
 { pkgs, ... }:
 {
   programs = {
-    steam.enable = true;
+    steam = {
+      enable = true;
+      gamescopeSession.enable = true;
+
+      package = pkgs.steam.override {
+        extraPkgs = pkgs:
+          with pkgs; [
+            xorg.libXcursor
+            xorg.libXi
+            xorg.libXinerama
+            xorg.libXScrnSaver
+            libpng
+            libpulseaudio
+            libvorbis
+            stdenv.cc.cc.lib
+            libkrb5
+            keyutils
+          ];
+      };
+    };
     gamemode.enable = true;
+    gamescope = {
+      enable = true;
+      capSysNice = true;
+    };
   };
   environment.systemPackages = with pkgs; [
     protonup-qt
