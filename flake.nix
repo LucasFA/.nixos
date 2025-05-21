264f45f288e9c6a40d98d82bb735c0c8dcd2a275
LucasFA <23667494+LucasFA@users.noreply.github.com>
2025-05-21 23:28:46 +0100

    update: nixos-unstable

--- flake.nix
+++ flake.nix
@@ -2,11 +2,11 @@
   description = "My first flake!";
 
   inputs = {
-    nixpkgs.url = "nixpkgs/nixos-25.05";
-    # nixpkgs.url = "nixpkgs/nixos-unstable";
+    # nixpkgs.url = "nixpkgs/nixos-25.05";
+    nixpkgs.url = "nixpkgs/nixos-unstable";
     home-manager = {
-      url = "github:nix-community/home-manager/release-25.05";
-      # url = "github:nix-community/home-manager";
+      # url = "github:nix-community/home-manager/release-25.05";
+      url = "github:nix-community/home-manager";
       inputs.nixpkgs.follows = "nixpkgs";
     };
     nixos-hardware.url = "github:LucasFA/nixos-hardware";
