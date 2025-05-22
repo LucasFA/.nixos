bcf65b0e0d39b82e7eff87679c93afa63ef333a3
LucasFA <23667494+LucasFA@users.noreply.github.com>
2025-05-22 14:31:53 +0100

    refactor/fix: 25.05 warning renamed option

--- home/common/vscode/default.nix
+++ home/common/vscode/default.nix
@@ -6,7 +6,7 @@
   programs.vscode = {
     enable = true;
     package = pkgs.vscodium;
-    extensions = with pkgs.vscode-extensions; [
+    profiles.default.extensions = with pkgs.vscode-extensions; [
       jnoortheen.nix-ide
       vscodevim.vim
       yzhang.markdown-all-in-one
