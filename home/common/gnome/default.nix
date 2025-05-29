c7defaf86cd2f47ffd2efd2b8937eb86f7452271
LucasFA <23667494+LucasFA@users.noreply.github.com>
2025-05-29 13:54:18 +0100

    disable localsend, enable kdeconnect

--- home/common/gnome/default.nix
+++ home/common/gnome/default.nix
@@ -1,6 +1,11 @@
-{ lib, ... }:
+{ pkgs, lib, ... }:
 {
   imports = [ ./dconf.nix ];
 
   services.caffeine.enable = true;
+  services.kdeconnect = {
+    # enable = true;
+    package = pkgs.gnomeExtensions.gsconnect;
+    indicator = true;
+  };
 }
