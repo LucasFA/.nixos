ed3e59e78227d75c35279f27de4d80c3420e0b1f
LucasFA <23667494+LucasFA@users.noreply.github.com>
2025-06-27 23:20:31 +0100

    server-nuc1: force tailscale, disable ssh, use server base-system module

--- hosts/nuc1/default.nix
+++ hosts/nuc1/default.nix
@@ -11,5 +11,10 @@
     ./configuration.nix
     ../../modules/WD_8tb
     ../../modules/core
+    ../../modules/base-systems/server
   ];
+
+  services.tailscale.enable = lib.mkForce true; # #### allowNoPasswordLogin allows to have no SSH keys for root or any
+  users.allowNoPasswordLogin = true; # ### wheel group user. Therefore, force tailscale: otherwise locked out
+
 }
