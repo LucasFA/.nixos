b50e38e456e0ff2bd04ef64337bb80ddac011faf
LucasFA <23667494+LucasFA@users.noreply.github.com>
2025-07-04 15:55:48 +0100

    slimbook: disable autologin

--- hosts/slimbook-hero/software/default.nix
+++ hosts/slimbook-hero/software/default.nix
@@ -27,11 +27,11 @@
   system.stateVersion = "24.05"; # Did you read the comment?
 
   # Enable automatic login for the user.
-  services.displayManager.autoLogin.enable = true;
+  services.displayManager.autoLogin.enable = false;
   services.displayManager.autoLogin.user = "lucasfa";
   services.displayManager.defaultSession = "gnome";
 
   # Workaround for GNOME autologin: https://github.com/NixOS/nixpkgs/issues/103746#issuecomment-945091229
-  systemd.services."getty@tty1".enable = false;
-  systemd.services."autovt@tty1".enable = false;
+  #systemd.services."getty@tty1".enable = false;
+  #systemd.services."autovt@tty1".enable = false;
 }
