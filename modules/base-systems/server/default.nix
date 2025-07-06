e16170e5f2a5d3d2fd6489c14c4c5b64a32839ae
LucasFA <23667494+LucasFA@users.noreply.github.com>
2025-07-06 20:10:22 +0100

    server: refactor ssh config

--- modules/base-systems/server/default.nix
+++ modules/base-systems/server/default.nix
@@ -1,5 +1,4 @@
 {
-  pkgs,
   lib,
   config,
   ...
@@ -9,24 +8,9 @@
   imports = [
     ../../borgbackup/server.nix
     # ../../forgejo
+    ./ssh.nix
   ];
   virtualisation.docker.enable = true;
-  services.fail2ban.enable = true;
-  services.openssh = {
-    enable = lib.mkForce true;
-    settings = {
-      PasswordAuthentication = false;
-      KbdInteractiveAuthentication = false;
-      PermitRootLogin = "no";
-      AllowUsers = [ "lucasfa" ];
-    };
-  };
-  users.users.lucasfa.openssh.authorizedKeys.keys = [
-    "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIA8s/0c98/d6Q6SkPTzKS0S7lm26uIywus/YNXKs3Ayp"
-  ];
   networking.networkmanager.ethernet.macAddress = "stable";
 
-  services.tailscale.enable = lib.mkForce true; # #### allowNoPasswordLogin allows to have no SSH keys for root or any
-  users.allowNoPasswordLogin = true; # ### wheel group user. Therefore, force tailscale: otherwise locked out
-  networking.firewall.allowedUDPPorts = [ config.services.tailscale.port ];
 }
---
+++ modules/base-systems/server/ssh.nix
@@ -0,0 +1,26 @@
+{
+  lib,
+  config,
+  ...
+}:
+{
+  # By default tailscale,
+  services.tailscale.enable = lib.mkForce true; # #### allowNoPasswordLogin allows to have no SSH keys for root or any
+  users.allowNoPasswordLogin = true; # ### wheel group user. Therefore, force tailscale: otherwise locked out
+  networking.firewall.allowedUDPPorts = [ config.services.tailscale.port ];
+
+  # but you can also log in through standard SSH. If no port forwarding in the router, at least in the LAN
+  services.fail2ban.enable = true;
+  services.openssh = {
+    enable = lib.mkForce true;
+    settings = {
+      PasswordAuthentication = false;
+      KbdInteractiveAuthentication = false;
+      PermitRootLogin = "no";
+      AllowUsers = [ "lucasfa" ];
+    };
+  };
+  users.users.lucasfa.openssh.authorizedKeys.keys = [
+    "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIA8s/0c98/d6Q6SkPTzKS0S7lm26uIywus/YNXKs3Ayp" # Slimbook
+  ];
+}
