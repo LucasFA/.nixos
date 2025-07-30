ad37f83d93a67acfc0ce4539170ca9b11ee599ef
LucasFA <23667494+LucasFA@users.noreply.github.com>
2025-07-31 00:54:26 +0100

    secrets

--- secrets/secrets.nix
+++ secrets/secrets.nix
@@ -5,13 +5,18 @@ let
     lucasfaKey
   ];
 
-  slimbook = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIHkSNFxbS5XMLVTLbCInuhO9DNVklEv22QAEcL5+4wHI"; # /etc/ssh/id_ed25519
+  # slimbook = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIHkSNFxbS5XMLVTLbCInuhO9DNVklEv22QAEcL5+4wHI"; # /etc/ssh/id_ed25519
   hp-omen = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIDSEUv/KiQ7b5JMCzL/muEYlSB5NB2+jb4mG1pDrikad"; # also on github.com/lucasfa.keys
   server-nuc1 = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAINk6Enh1qpGbOCCH71KHVDiutXYGtra9SVKbaQbY86ZL"; # /etc/ssh/ssh_host_ed25519
   systems = [
-    slimbook
+    # slimbook
     hp-omen
     server-nuc1
+  ];
+  backupKeys = [
+    # slimbook
+    server-nuc1
+    lucasfaKey
   ];
 in
 {
@@ -20,7 +25,7 @@ in
     lucasfa-server-nuc1
     slimbook
   ];
-  "restic/env.age".publicKeys = [ slimbook ];
-  "restic/repo.age".publicKeys = [ slimbook ];
-  "restic/applicationKey.age".publicKeys = [ slimbook ];
+  "restic/env.age".publicKeys = [ backupKeys ];
+  "restic/repo.age".publicKeys = [ backupKeys ];
+  "restic/applicationKey.age".publicKeys = [ backupKeys ];
 }
