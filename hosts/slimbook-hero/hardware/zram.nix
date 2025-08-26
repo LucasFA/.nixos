b959a332b43b71972fb479c4dc9ffd6e5b4bb2ad
LucasFA <23667494+LucasFA@users.noreply.github.com>
2025-08-27 00:05:01 +0100

    slimbook-hero: zram sysctl watermark typo

--- hosts/slimbook-hero/hardware/zram.nix
+++ hosts/slimbook-hero/hardware/zram.nix
@@ -14,6 +14,6 @@
   boot.kernel.sysctl = {
     "vm.page-cluster" = 0;
     "vm.swappiness" = 150;
-    "vm.watermark_scaling_factor" = 100; # See https://github.com/pop-os/default-settings/pull/163
+    "vm.watermark_scale_factor" = 100; # See https://github.com/pop-os/default-settings/pull/163
   };
 }
