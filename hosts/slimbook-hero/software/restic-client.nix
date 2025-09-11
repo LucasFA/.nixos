b1cb8fca12d830a4c71359fb31151d6a20da14af
LucasFA <23667494+LucasFA@users.noreply.github.com>
2025-09-11 02:02:46 +0100

    slimbook: restic-client: monthly check --read-data

--- hosts/slimbook-hero/software/restic-client.nix
+++ hosts/slimbook-hero/software/restic-client.nix
@@ -58,6 +58,9 @@ let
     Persistent = true;
     RandomizedDelaySec = "1h";
   };
+  monthlyTimer = defaultTimer // {
+    OnCalendar = "monthly";
+  };
   pruneOpts = [
     "--keep-daily 7"
     "--keep-weekly 5"
@@ -77,11 +80,19 @@ let
     pruneOpts = pruneOpts;
     runCheck = true;
     # Backblaze B2 pricing: 1G daily free egress + 300% of average monthly storage free egress # (IDK which it is...)
-    checkOpts = [ "--read-data-subset=5%" ];
+    checkOpts = [ ];
     timerConfig = defaultTimer;
     extraBackupArgs = defaultExtraBackupArgs;
-
   };
+
+  mkMonthlyCheckJob =
+    job:
+    job
+    // {
+      timerConfig = monthlyTimer;
+      checkOpts = [ "--read-data" ];
+    };
+
   defaultNotifySet = {
     enable = true;
     description = "Notify on failed backup";
@@ -110,6 +121,16 @@ let
     owner = "lucasfa";
     group = "users";
   };
+  backblazeBackupJob = backupJobTemplate // {
+    repository = "s3:https://s3.eu-central-003.backblazeb2.com/lucasfa-backups";
+    environmentFile = config.age.secrets."restic/backblazeCredentials".path;
+  };
+
+  nuc1BackupJob = backupJobTemplate // {
+    repository = "rest:https://restic.lucasfa.com/lucasfa/slimbook-laptop";
+    environmentFile = config.age.secrets."restic/environmentFile".path;
+    progressFps = 0.02;
+  };
 in
 {
   # environment.systemPackages = with pkgs; [ restic ];
@@ -126,21 +147,16 @@ in
   };
 
   systemd.services.restic-backups-nuc1.environment.GOMAXPROCS = "8";
+  systemd.services.restic-backups-nuc1-check.environment.GOMAXPROCS = "8";
   systemd.services.restic-backups-backblaze.environment.GOMAXPROCS = "8";
 
   # To run restic on a shell use the NixOS provided wrapper: `restic-<name>`, where <name>
   # is services.restic.backups.<name>. For example, restic-nuc1 or restic-backblaze
   services.restic.backups = {
-    backblaze = backupJobTemplate // {
-      repository = "s3:https://s3.eu-central-003.backblazeb2.com/lucasfa-backups";
-      environmentFile = config.age.secrets."restic/backblazeCredentials".path;
-    };
-
-    nuc1 = backupJobTemplate // {
-      repository = "rest:https://restic.lucasfa.com/lucasfa/slimbook-laptop";
-      environmentFile = config.age.secrets."restic/environmentFile".path;
-      progressFps = 0.02;
-    };
+    backblaze = backblazeBackupJob;
+    backblaze-check = mkMonthlyCheckJob backblazeBackupJob;
+    nuc1 = nuc1BackupJob;
+    nuc1-check = mkMonthlyCheckJob nuc1BackupJob;
   };
 
   # Notification in case of failures
