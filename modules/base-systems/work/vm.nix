9fac526e103e9a9f3aa61b4b6957344ad2c4565d
LucasFA <23667494+LucasFA@users.noreply.github.com>
2025-10-07 16:03:23 +0200

    work: disable vm for update

--- modules/base-systems/work/vm.nix
+++ modules/base-systems/work/vm.nix
@@ -4,12 +4,14 @@
   config,
   ...
 }:
+let
+  virtEnabled = config.virtualisation.libvirtd.enable;
+in
 {
-  virtualisation.libvirtd.enable = true;
-  programs.virt-manager.enable = true;
-  users.users.lucasfa.extraGroups = [ "libvirtd" ];
+  programs.virt-manager.enable = lib.mkIf virtEnabled true;
+  users.users.lucasfa.extraGroups = lib.mkIf virtEnabled [ "libvirtd" ];
   virtualisation.libvirtd = {
-    # enable = lib.mkForce true;
+    enable = false;
     qemu = {
       package = pkgs.qemu_kvm;
       ovmf = {
@@ -19,17 +21,19 @@
       swtpm.enable = true;
     };
   };
-  environment.systemPackages = with pkgs; [
-    quickemu
-    socat
-    virt-manager
-    virt-viewer
-    spice
-    spice-gtk
-    spice-protocol
-    win-virtio
-    win-spice
-    adwaita-icon-theme
-  ];
+  environment.systemPackages =
+    with pkgs;
+    lib.mkIf virtEnabled [
+      quickemu
+      socat
+      virt-manager
+      virt-viewer
+      spice
+      spice-gtk
+      spice-protocol
+      win-virtio
+      win-spice
+      adwaita-icon-theme
+    ];
 
 }
