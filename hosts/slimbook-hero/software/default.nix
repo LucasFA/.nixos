7555a18727482ee6a0f1ed82fc3b970d23b3b28a
LucasFA <23667494+LucasFA@users.noreply.github.com>
2025-10-27 11:08:01 +0100

    nix fmt

--- hosts/slimbook-hero/software/default.nix
+++ hosts/slimbook-hero/software/default.nix
@@ -28,7 +28,6 @@
   # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
   system.stateVersion = "24.05"; # Did you read the comment?
 
-
   # Workaround for GNOME autologin: https://github.com/NixOS/nixpkgs/issues/103746#issuecomment-945091229
   #systemd.services."getty@tty1".enable = false;
   #systemd.services."autovt@tty1".enable = false;
