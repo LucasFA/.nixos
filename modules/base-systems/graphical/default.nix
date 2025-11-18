3cdd7619a1dda28a4a71daced73b6e1451dcdefc
LucasFA <23667494+LucasFA@users.noreply.github.com>
2025-11-18 12:41:00 +0100

    kill X11

--- modules/base-systems/graphical/default.nix
+++ modules/base-systems/graphical/default.nix
@@ -25,7 +25,4 @@
   ];
 
   services.earlyoom.enable = true;
-
-  # Enable the X11 windowing system.
-  services.xserver.enable = true; # Yeah, no. Even using Wayland I need this. Just goes to TTY not an issue
 }
