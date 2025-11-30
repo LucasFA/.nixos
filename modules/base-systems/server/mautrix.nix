569da38a1477f76fee36e02d7f9e99fd8cd76027
LucasFA <23667494+LucasFA@users.noreply.github.com>
2025-11-30 18:49:54 +0000

    mautrix

--- modules/base-systems/server/mautrix.nix
+++ modules/base-systems/server/mautrix.nix
@@ -1,12 +1,20 @@
 {
   lib,
+  pkgs,
   config,
   ...
 }:
 
+let
+ mautrix-signalGoolm = pkgs.mautrix-signal.override { withGoolm = true; };
+in 
 {
   services.mautrix-signal = {
     enable = true;
+    package = mautrix-signalGoolm;
   };
+  environment.systemPackages = with pkgs; [
+    mautrix-signalGoolm
+  ];
 
 }
