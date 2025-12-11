9945c4e980a74ee636733a6220abdcdb97fed4f4
LucasFA <23667494+LucasFA@users.noreply.github.com>
2025-12-11 22:15:03 +0100

    server: wip branch parity

--- modules/base-systems/server/mautrix.nix
+++ modules/base-systems/server/mautrix.nix
@@ -1,20 +1,12 @@
 {
   lib,
-  pkgs,
   config,
   ...
 }:
 
-let
- mautrix-signalGoolm = pkgs.mautrix-signal.override { withGoolm = true; };
-in 
 {
   services.mautrix-signal = {
     enable = true;
-    package = mautrix-signalGoolm;
   };
-  environment.systemPackages = with pkgs; [
-    mautrix-signalGoolm
-  ];
 
 }
