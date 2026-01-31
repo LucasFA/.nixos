377276219ffe80560cbe0d20da8783cac78d8c3f
LucasFA <23667494+LucasFA@users.noreply.github.com>
2026-01-31 20:58:44 +0100

    install easyeffects

--- home/common/audio.nix
+++ home/common/audio.nix
@@ -1,9 +1,9 @@
 { config, pkgs, ... }:
 {
-  #services.easyeffects.enable = true;
+  services.easyeffects.enable = true;
   home.packages = with pkgs; [
-    # gnomeExtensions.easyeffects-preset-selector
+    lib.optional
+    config.services.desktopManager.gnome.enable
+    gnomeExtensions.easyeffects-preset-selector
   ];
-  #lib.optional config.services.desktopManager.gnome.enable
-  #gnomeExtensions.easyeffects-preset-selector;
 }
