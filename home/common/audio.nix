cf65358d70311de701006ee25ecc2adbfb141ad8
LucasFA <23667494+LucasFA@users.noreply.github.com>
2026-02-03 17:21:16 +0100

    easyeffects: uninstall

--- home/common/audio.nix
+++ home/common/audio.nix
@@ -1,9 +1,7 @@
 { config, pkgs, ... }:
 {
-  services.easyeffects.enable = true;
+  #services.easyeffects.enable = true;
   home.packages = with pkgs; [
-    lib.optional
-    config.services.desktopManager.gnome.enable
-    gnomeExtensions.easyeffects-preset-selector
+    #gnomeExtensions.easyeffects-preset-selector
   ];
 }
