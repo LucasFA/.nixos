27aa66abd77624a4371a325e06a033916759ff0e
LucasFA <23667494+LucasFA@users.noreply.github.com>
2026-03-02 21:17:08 +0100

    nix fmt

--- modules/base-systems/gaming/default.nix
+++ modules/base-systems/gaming/default.nix
@@ -1,4 +1,9 @@
-{ lib, pkgs, ... }:
+{
+  config,
+  lib,
+  pkgs,
+  ...
+}:
 
 let
   patchedPkgs = import pkgs.path {
@@ -7,23 +12,23 @@ let
       #(self: super: {
       #stdenv = super.stdenvAdapters.impureUseNativeOptimizations super.stdenv;
       #})
-/*
-      (self: super: {
-        rpcs3 = super.rpcs3.overrideAttrs (old: {
-          patches = (old.patches or [ ]) ++ [
-            (super.fetchpatch {
-              name = "fix-build-qt-6.10.patch";
-              url = "https://github.com/RPCS3/rpcs3/commit/038ee090b731bf63917371a3586c2f7d7cf4e585.patch";
-              hash = "sha256-jTIxsheG9b9zp0JEeWQ73BunAXmEIg5tj4SrWBfdHy8=";
+      /*
+            (self: super: {
+              rpcs3 = super.rpcs3.overrideAttrs (old: {
+                patches = (old.patches or [ ]) ++ [
+                  (super.fetchpatch {
+                    name = "fix-build-qt-6.10.patch";
+                    url = "https://github.com/RPCS3/rpcs3/commit/038ee090b731bf63917371a3586c2f7d7cf4e585.patch";
+                    hash = "sha256-jTIxsheG9b9zp0JEeWQ73BunAXmEIg5tj4SrWBfdHy8=";
+                  })
+                  (super.fetchpatch {
+                    url = "https://github.com/LucasFA/rpcs3/commit/5046d08c55bbb693f438336cca00d92bb08a68d5.patch";
+                    sha256 = "sha256-HVbipN+vqQ8qAiKnNyMT8Mt1+J0CLziSHvyzEb2W7dQ=";
+                  })
+                ];
+              });
             })
-            (super.fetchpatch {
-              url = "https://github.com/LucasFA/rpcs3/commit/5046d08c55bbb693f438336cca00d92bb08a68d5.patch";
-              sha256 = "sha256-HVbipN+vqQ8qAiKnNyMT8Mt1+J0CLziSHvyzEb2W7dQ=";
-            })
-          ];
-        });
-      })
-*/
+      */
     ];
   };
 in
