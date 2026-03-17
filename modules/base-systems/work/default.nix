bef2863613ae0c3d7ef5e53cae55f500413a958a
LucasFA <23667494+LucasFA@users.noreply.github.com>
2026-03-17 11:28:31 +0100

    work base-system reorg

--- modules/base-systems/work/default.nix
+++ modules/base-systems/work/default.nix
@@ -4,12 +4,32 @@
   config,
   ...
 }:
-
+let
+  virtEnabled = false;
+in
 {
-  imports = [
-    ./vm.nix
-  ];
-  environment.systemPackages = with pkgs; [
-    gnome-boxes
-  ];
+  virtualisation.libvirtd = {
+    enable = virtEnabled;
+    qemu = {
+      package = pkgs.qemu_kvm;
+      swtpm.enable = true;
+    };
+  };
+  programs.virt-manager.enable = lib.mkIf virtEnabled true;
+  users.users.lucasfa.extraGroups = lib.mkIf virtEnabled [ "libvirtd" ];
+
+  environment.systemPackages =
+    with pkgs;
+    lib.mkIf virtEnabled [
+      gnome-boxes
+      quickemu
+      socat
+      virt-manager
+      virt-viewer
+      spice
+      spice-gtk
+      spice-protocol
+      adwaita-icon-theme
+    ];
+
 }
