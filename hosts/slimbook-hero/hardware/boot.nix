aa30419444dcacbcb8810073b629b39b77e81a60
LucasFA <23667494+LucasFA@users.noreply.github.com>
2026-03-24 23:19:10 +0100

    slimbook-hero: boot: remove grub

--- hosts/slimbook-hero/hardware/boot.nix
+++ hosts/slimbook-hero/hardware/boot.nix
@@ -26,24 +26,4 @@ in
   boot.kernel.sysctl."kernel.sysrq" = 1;
 
   boot.kernelParams = [ ];
-
-  boot.loader.grub = {
-    enable = false;
-    copyKernels = false; # no need: uses them straight from /nix/store
-    configurationLimit = confLimit;
-    useOSProber = true;
-    efiSupport = true;
-    #efiInstallAsRemovable = true; # in case canTouchEfiVariables doesn't work for your system
-    device = "nodev";
-    font = "${pkgs.fira-code}/share/fonts/truetype/FiraCode-VF.ttf";
-    fontSize = 36;
-    extraEntries = ''
-                      menuentry "Reboot" {
-                          reboot
-                      }
-       		menuentry "Poweroff" {
-                          halt
-                      }
-      		'';
-  };
 }
