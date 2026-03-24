fa3c59d8e3d1c2539af69540fc992f3c14e630f6
LucasFA <23667494+LucasFA@users.noreply.github.com>
2026-03-25 00:07:06 +0100

    hosts/form-factor: delete

--- hosts/form-factor/laptop/default.nix
+++
@@ -1,5 +0,0 @@
-{ lib, ... }:
-{
-  # services.automatic-timezoned.enable = lib.mkDefault true; # never got it to work
-  time.timeZone = lib.mkForce null; # set by user
-}
--- hosts/slimbook-hero/default.nix
+++ hosts/slimbook-hero/default.nix
@@ -1,12 +1,15 @@
 {
   pkgs,
+  lib,
   ...
 }:
 
 {
   imports = [
-    ../form-factor/laptop
     ./hardware
     ./software
   ];
+
+  # services.automatic-timezoned.enable = lib.mkDefault true; # never got it to work
+  time.timeZone = lib.mkForce null; # set by user
 }
