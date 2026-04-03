9f67e2fdbb7df193941403257349089db6a0710a
LucasFA <23667494+LucasFA@users.noreply.github.com>
2026-04-03 12:04:17 +0100

    home/zed: add mailbox fix for multimonitor

--- modules/home/zed.nix
+++ modules/home/zed.nix
@@ -13,5 +13,12 @@
         ui_font_size = 16;
       };
     };
+
+    home.packages = [
+      (pkgs.writeShellScriptBin "zeditor" ''
+        export MESA_VK_WSI_PRESENT_MODE=mailbox
+        exec ${pkgs.zed-editor}/bin/zeditor "$@"
+      '')
+    ];
   };
 }
