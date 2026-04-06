1f48cdd33ffe75f8b54f92a3d13673a1c9c94429
LucasFA <23667494+LucasFA@users.noreply.github.com>
2026-04-06 20:26:50 +0200

    core/locale: add LC_MESSAGES

--- modules/core/default.nix
+++ modules/core/default.nix
@@ -173,6 +173,7 @@
     i18n.defaultLocale = "en_US.UTF-8";
 
     i18n.extraLocaleSettings = {
+      LC_MESSAGES = "en_US.UTF-8";
       LC_ADDRESS = "es_ES.UTF-8";
       LC_IDENTIFICATION = "es_ES.UTF-8";
       LC_MEASUREMENT = "es_ES.UTF-8";
