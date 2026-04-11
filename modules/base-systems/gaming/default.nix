b4b1589c0a600865c78fae462753e9766ec04d77
LucasFA <23667494+LucasFA@users.noreply.github.com>
2026-04-11 13:50:48 +0200

    desktop/gaming: rpcs3 overlay

--- modules/base-systems/gaming/default.nix
+++ modules/base-systems/gaming/default.nix
@@ -5,33 +5,6 @@
   ...
 }:
 
-let
-  patchedPkgs = import pkgs.path {
-    inherit (pkgs) system config;
-    overlays = [
-      #(self: super: {
-      #stdenv = super.stdenvAdapters.impureUseNativeOptimizations super.stdenv;
-      #})
-      /*
-            (self: super: {
-              rpcs3 = super.rpcs3.overrideAttrs (old: {
-                patches = (old.patches or [ ]) ++ [
-                  (super.fetchpatch {
-                    name = "fix-build-qt-6.10.patch";
-                    url = "https://github.com/RPCS3/rpcs3/commit/038ee090b731bf63917371a3586c2f7d7cf4e585.patch";
-                    hash = "sha256-jTIxsheG9b9zp0JEeWQ73BunAXmEIg5tj4SrWBfdHy8=";
-                  })
-                  (super.fetchpatch {
-                    url = "https://github.com/LucasFA/rpcs3/commit/5046d08c55bbb693f438336cca00d92bb08a68d5.patch";
-                    sha256 = "sha256-HVbipN+vqQ8qAiKnNyMT8Mt1+J0CLziSHvyzEb2W7dQ=";
-                  })
-                ];
-              });
-            })
-      */
-    ];
-  };
-in
 {
   users.users.lucasfa.extraGroups = lib.mkIf config.programs.gamemode.enable [ "gamemode" ];
   boot.kernelModules = [ "ntsync" ];
--- modules/roles/desktop/gaming.nix
+++ modules/roles/desktop/gaming.nix
@@ -6,6 +6,24 @@
 }:
 let
   cfg = config.lfa.roles.desktop;
+  patchedPkgs = import pkgs.path {
+    inherit (pkgs) system config;
+    overlays = [
+      #(self: super: {
+      #stdenv = super.stdenvAdapters.impureUseNativeOptimizations super.stdenv;
+      #})
+      (self: super: {
+        rpcs3 = super.rpcs3.overrideAttrs (old: {
+          patches = (old.patches or [ ]) ++ [
+            (super.fetchpatch {
+              url = "https://github.com/LucasFA/rpcs3/commit/5046d08c55bbb693f438336cca00d92bb08a68d5.patch";
+              sha256 = "sha256-HVbipN+vqQ8qAiKnNyMT8Mt1+J0CLziSHvyzEb2W7dQ=";
+            })
+          ];
+        });
+      })
+    ];
+  };
 in
 {
   config = lib.mkIf cfg.enable {
