8ae2232283ef12256b96e2158f5bacf53e05e8c2
LucasFA <23667494+LucasFA@users.noreply.github.com>
2026-05-01 00:14:42 +0200

    roles/desktop/laptop: add boot systemd service to set ppd profile

--- modules/roles/desktop/laptop.nix
+++ modules/roles/desktop/laptop.nix
@@ -9,6 +9,34 @@ let
 in
 {
   config = lib.mkIf cfg.enable {
+    systemd.services =
+      let
+        defaultService = {
+          enable = true;
+          description = "Update the power-profiles-daemon perf mode on boot";
+          after = [ "multi-user.target" ];
+          wantedBy = [ "multi-user.target" ];
+          serviceConfig = {
+            Type = "oneshot";
+            User = config.users.users.lucasfa.name;
+          };
+          script = ''
+            	AC_PATH="/sys/class/power_supply/AC/online"
+            	AC_STATE=$(cat "/sys/class/power_supply/AC0/online")
+                case $AC_STATE in
+                    "1")
+                        "${pkgs.power-profiles-daemon}/bin/powerprofilesctl set performance"
+                        ;;
+                    "0")
+                        "${pkgs.power-profiles-daemon}/bin/powerprofilesctl set power-saver"
+                        ;;
+                esac
+          '';
+        };
+      in
+      {
+        "set-ppd-profile-on-boot" = defaultService;
+      };
     # stolen from https://github.com/Straffern/config/blob/a1013f58375ea49529660a970c5d8b22f78b5272/modules/nixos/system/battery/default.nix#L74
     services.udev.extraRules = lib.mkIf config.services.power-profiles-daemon.enable ''
       # When AC adapter is plugged in - switch to performance
