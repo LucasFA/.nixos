aac7ce3658b16788e91393d7503fecf028e44112
LucasFA <23667494+LucasFA@users.noreply.github.com>
2026-09-08 19:20:53 +0100

    node804: enable zswap

--- hosts/node804/default.nix
+++ hosts/node804/default.nix
@@ -7,9 +7,6 @@
   config,
   ...
 }:
-let
-  dduper = pkgs.callPackage ./dduper.nix { };
-in
 {
   imports = [
     ./hardware-configuration.nix
@@ -21,14 +18,19 @@ in
   ];
   lfa.hostRole = hostRole;
 
-  environment.systemPackages = [
-    #dduper
-  ];
-
   services.udisks2.enable = true;
   # As there is no DE, this has to be manually set in order to install intel-media-driver et al
   hardware.graphics.enable = true;
   networking.firewall = {
     allowedUDPPorts = [ 51820 ]; # Clients and peers can use the same port, see listenport. Wireguard
   };
+
+  boot.kernelParams = [
+    "zswap.enabled=1"
+    "zswap.shrinker_enabled=1" # whether to shrink the pool proactively on high memory pressure
+
+    # defaults:
+    # "zswap.compressor=zstd"
+    # "zswap.max_pool_percent=20" # maximum percentage of RAM that zswap is allowed to use
+  ];
 }
