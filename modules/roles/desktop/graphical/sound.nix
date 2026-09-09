26af95f04b4190c275a52330a90a3dece2e41726
LucasFA <23667494+LucasFA@users.noreply.github.com>
2026-09-09 18:04:31 +0100

    desktop: add pipewire switch-on-connect

--- modules/roles/desktop/graphical/sound.nix
+++ modules/roles/desktop/graphical/sound.nix
@@ -15,6 +15,14 @@ in
       alsa.enable = true;
       alsa.support32Bit = true;
       pulse.enable = true;
+      extraConfig.pipewire-pulse."10-switch-on-connect" = {
+        "pulse.cmd" = [
+          {
+            cmd = "load-module";
+            args = "module-switch-on-connect";
+          }
+        ];
+      };
       # If you want to use JACK applications, uncomment this
       #jack.enable = true;
 
